51dbe3a65e734a7bb618ed3dd88e78ba8adb4c57
Kuba Gretzky <kuba@breakdev.org>
2018-03-05 19:22:23 +0100

    RTFM caplet that can tell when you did not RTFM added

---
+++ rtfm.js
@@ -0,0 +1,22 @@
+
+function onRequest(req, res) {
+    // clear caching headers
+    for( var i = 0; i < req.Headers.length; ) {
+        var header = req.Headers[i];
+        if (header.Name == "If-Modified-Since" || header.Name == "If-None-Match") {
+            req.Headers.splice(i, 1);
+        }
+        else {
+            i++;
+        }
+    }
+}
+
+function onResponse(req, res) {
+    if (res.ContentType.indexOf("image/jpeg") != -1) {
+        res.Status = 200;
+        res.Headers     = "Connection: close";
+        res.Body = readFile("caplets/www/rtfm_cat.jpg");
+        log("RTFM! " + req.Hostname + req.Path + ( req.Query ? "?" + req.Query : ''));
+    }
+}
